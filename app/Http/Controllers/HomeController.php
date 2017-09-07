e345d2a3d5dc75ad0c3070323e63cbd4367ab1b1
woctober <xiangjiao0515@163.com>
2017-09-07 17:36:21 +0800

    revise paging

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,25 +19,21 @@ class HomeController extends BaseController
         $jsVars = [
             ['user', \Globals::$user]
         ];
-        // $page = $request->input('page');
-        // $pagesize = $request->input('pagesize');
-        // $validate = Validator::make($request->all(), [
-        //     'page'   => 'required',
-        //     'pagesize' => 'required',
-        //     ]);
-        
-        // $jsVars = [
-        //     ['user', \Globals::$user]
-        // ];
-        // $uid = \Globals::$user['id'];
-        // $tags = User::selectTagsById($uid);
-        // $tags_arr = json_decode($tags, true);
-        // $tag = $tags_arr[0];
-        // $url = Photo::getPhotoByTags($tag, $page, $pagesize = 2);
-        // return $url;
+
+        //若要显示首页，将此处至return $url处注释即可；
+        $page = $request->input('page');
+        $validate = Validator::make($request->all(), [
+            'page'   => 'required',
+            ]);
+        $uid = \Globals::$user['id'];
+        $tags = User::selectTagsById($uid);
+        $tags_arr = json_decode($tags, true);
+        $tag = $tags_arr[0];
+        $url = Photo::getPhotoByTags($tag, $page, $pagesize = 8);
+        return $url;
 
         return view('home')
-        ->with('jsVars', $jsVars)
-        ->with('hotSearch', '热门');
+            ->with('jsVars', $jsVars)
+            ->with('hotSearch', '热门');
     }
 }
--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -3,20 +3,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Salt;
-use DB;
 
 class Photo extends Model
 {
     protected $table = 'photo';
+
+    const PAGE = 2;
+    const PAGESIZE = 10;
+    const DEFAULTSIZE = 8;
 
 	public static function getPhotoByTags($tag, $page, $pagesize = 1) {
         $skip = 0;
         if ($page < 1) {
         	return false;
         }
-        if ($page >= 2) {
-            $skip = 2 + 3 * ($page - 2);
-            $pagesize = 3;
+        if ($page >= self::PAGE) {
+            $skip = self::DEFAULTSIZE + self::PAGESIZE * ($page - self::PAGE);
+            $pagesize = self::PAGESIZE;
         }
         $res = static::where($tag, 1)
                             ->skip($skip)
